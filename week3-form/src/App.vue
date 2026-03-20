5a42c55fca00a5fc4587c31161f0a340d5ec4a3e
uncledrew-sr <bonggutravel@gmail.com>
2026-03-20 14:17:53 +0900

    week3 complete

---
+++ week3-form/src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from 'vue';
+
+const userInfo = ref({
+  username: '',
+  gender: 'male',
+  hobbies: []
+});
+</script>
+
+<template>
+  <h1>3주차 실습 시작</h1>
+  <div class="container">
+    <h2>실시간 회원가입 폼</h2>
+    <div class="field">
+      <label>이름: </label>
+      <input type="text" v-model="userInfo.username" placeholder="이름 입력">
+    </div>
+    <div class="field">
+      <label>성별: </label>
+      <label><input type="radio" name="gender" value="male" v-model="userInfo.gender">남성</label>
+      <label><input type="radio" name="gender" value="female" v-model="userInfo.gender">여성</label>
+    </div>
+    <div class="field">
+      <label>취미: </label>
+      <label><input type="checkbox" name="hobbies" value="프로그래밍" v-model="userInfo.hobbies">프로그래밍</label>
+      <label><input type="checkbox" name="hobbies" value="농구" v-model="userInfo.hobbies">농구</label>
+      <label><input type="checkbox" name="hobbies" value="러닝" v-model="userInfo.hobbies">러닝</label>
+      <label><input type="checkbox" name="hobbies" value="테니스" v-model="userInfo.hobbies">테니스</label>
+    </div>
+    <hr>
+    <h3>실시간 데이터 반영 결과:</h3>
+    <pre class="result-box">{{ userInfo }}</pre>
+  </div>
+</template>
+
+<style scoped>
+  .container{
+    padding: 20px;
+    font-family: sans-serif;
+    max-width: 400px;
+    margin: 0 auto;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+  }
+  .field{
+    margin-bottom: 20px;
+  }
+  label{
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  .result-box{
+    background: #282c34;
+    color: #abb2bf;
+    padding: 15px;
+    border-radius: 8px;
+    font-weight: bold;
+  }
+</style>
